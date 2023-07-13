ea8ada772e869ba99861b7236195415dfc9e7919
shaphat-codes <jehoshaphattatiglo99@gmail.com>
2023-07-13 23:03:10 +0000

    added eroor page

--- waitlist_form/models.py
+++ waitlist_form/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class WaitlistForm(models.Model):
     full_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    phone = models.CharField(max_length=255, unique=True)
